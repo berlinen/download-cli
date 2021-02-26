9d7bfffac2eeafc8722ea2407177d0a0c00b6685
“berlinen” <1292393108@qq.com>
2021-02-26 21:54:08 +0800

    feat: func copy copyDir

--- utils.js
+++ utils.js
@@ -19,9 +19,38 @@ const emptyDir = (dir) => {
 
   }
 }
+/**
+ * @desc 将src拷贝到dest
+ * @param {*} src 文件目录
+ * @param {*} dest
+ */
+const copy = (src, dest) => {
+  const stat = fs.statSync(src);
+  if (stat.isDirectory) {
+    copyDir(src, dest);
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+};
+/**
+ * @desc 将srcDir目录拷贝到destDir
+ * @param {*} srcDir 文件目录
+ * @param {*} destDir
+ */
+const copyDir = (srcDir, destDir) => {
+  // 创建destDir文件目录
+  fs.mkdirSync(destDir, { recusive: true }); // 回调回接收创建的第一个路径作为参数
+  // 将srcDir文件复制到Dest sDir
+  for(const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file);
+    const destFile = path.resolve(destDir, file);
+    copy(srcFile, destFile);
+  };
+}
 
-const copy = ()
+
 
 module.exports = {
-  emptyDir
+  emptyDir,
+  copy
 }
